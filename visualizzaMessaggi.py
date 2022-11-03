c6f4cbe4bac4a6156a8856ab912e5627f068e67a
Camilla Giaccari <cami.lla@me.com>
2022-11-03 16:39:04 +0100

    Resi universali i nomi dei file csv

--- visualizzaMessaggi.py
+++ visualizzaMessaggi.py
@@ -1,9 +1,10 @@
+from st_aggrid import GridUpdateMode, DataReturnMode
 import streamlit as st
 import pandas as pd
 
 ###############################################################################
 # ATTENZIONE INSTALLARE IL COMPONENTE streamlit-aggrid CON IL SOTTOSTANTE COMANDO
-# pip install streamlit-aggrid 
+# pip install streamlit-aggrid
 ###############################################################################
 
 from st_aggrid import AgGrid
@@ -24,40 +25,41 @@ def _max_width_():
         unsafe_allow_html=True,
     )
 
+
 st.set_page_config(page_icon=":smile:", page_title="Whatsapp2.0")
 st.image("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/balloon_1f388.png", width=100)
 st.title("Whatsapp2.0 - Visualizza messaggi")
 
-utente=st.text_input('Nome utente')
-password=st.text_input('Password', type="password") 
+utente = st.text_input('Nome utente')
+password = st.text_input('Password', type="password")
 
-col1, col2= st.columns(2)
+col1, col2 = st.columns(2)
 
-richiestoAccesso=False
-richiestaRegistrazione=False
+richiestoAccesso = False
+richiestaRegistrazione = False
 
-from st_aggrid import GridUpdateMode, DataReturnMode
 
 with col1:
     if st.button('Accedi'):
-        richiestoAccesso=True
+        richiestoAccesso = True
 
 with col2:
     if st.button('Registrati'):
-        richiestaRegistrazione=True
+        richiestaRegistrazione = True
 
-nomeFileUtenti="C:\\224MI\\Progetti\\Whatsapp2.0\\utenti.csv"
+nomeFileUtenti = "utenti.csv"
 listaUtenti = pd.read_csv(nomeFileUtenti)
 
-if ('accessoEseguito' in st.session_state 
-    or (richiestoAccesso and utente in listaUtenti['Amici'].unique())):      
-    
+if ('accessoEseguito' in st.session_state
+        or (richiestoAccesso and utente in listaUtenti['Amici'].unique())):
+
     if 'accessoEseguito' not in st.session_state:
         st.session_state['accessoEseguito'] = [True]
-    
+
     gb = GridOptionsBuilder.from_dataframe(listaUtenti)
     # enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
-    gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
+    gb.configure_default_column(
+        enablePivot=True, enableValue=True, enableRowGroup=True)
     gb.configure_selection(selection_mode="multiple", use_checkbox=True)
     gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
     gridOptions = gb.build()
@@ -70,32 +72,33 @@ if ('accessoEseguito' in st.session_state
         data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
         fit_columns_on_grid_load=False,
     )
-    
-    if len(response['selected_rows'])>0:
-        destinatarioMessaggio=response['selected_rows'][0].get('Amici')
-        
-        nomeFileMessaggi="C:\\224MI\\Progetti\\Whatsapp2.0\\messaggi.csv"
+
+    if len(response['selected_rows']) > 0:
+        destinatarioMessaggio = response['selected_rows'][0].get('Amici')
+
+        nomeFileMessaggi = "messaggi.csv"
         listaMessaggi = pd.read_csv(nomeFileMessaggi)
-        
+
         # for col in listaMessaggi.columns:
         #     st.text(col)
-        nomeColDestinatario=listaMessaggi.columns[0]
-        nomeColMittente=listaMessaggi.columns[1]
-        listaMessaggiFiltrata=listaMessaggi.loc[listaMessaggi[nomeColDestinatario] == destinatarioMessaggio]
+        nomeColDestinatario = listaMessaggi.columns[0]
+        nomeColMittente = listaMessaggi.columns[1]
+        listaMessaggiFiltrata = listaMessaggi.loc[listaMessaggi[nomeColDestinatario]
+                                                  == destinatarioMessaggio]
         st.subheader("La chat selezionata apparirà qui sotto 👇 ")
         st.table(listaMessaggiFiltrata[listaMessaggi.columns])
 else:
-    if utente!='' and utente not in listaUtenti['Amici'].unique():
+    if utente != '' and utente not in listaUtenti['Amici'].unique():
         st.warning('Utente non censito in archivio', icon="⚠️")
 
 if richiestaRegistrazione:
     if utente in listaUtenti['Amici'].unique():
         st.warning('Utente già censito in archivio', icon="⚠️")
     else:
-        if password=='' :
+        if password == '':
             st.warning('Utente/Password obbligatori', icon="⚠️")
         else:
-            f=open(nomeFileUtenti,'a')
+            f = open(nomeFileUtenti, 'a')
             f.write('\n'+utente)
             f.close()
             st.warning('Utente inserito in archivio')
